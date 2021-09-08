e542aacf03e82c0120536fabe6ff229f42ed3ba8
Cameron Bass <cam@collectiveidea.com>
2021-09-08 07:45:37 -0400

    Add the AddTicker command to the tool

--- cmd/root.go
+++ cmd/root.go
@@ -18,8 +18,8 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"github.com/spf13/cobra"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
@@ -30,14 +30,14 @@ var rootCmd = &cobra.Command{
 	Use:   "quick_ticker",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+				 examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+				 Cobra is a CLI library for Go that empowers applications.
+				 This application is a tool to generate the needed files
+				 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) { fmt.Println("Hello World") },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
